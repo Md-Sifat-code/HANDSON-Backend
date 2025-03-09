64502435585c7f12bb840fd0be5a9c41f43d7831
Md-Sifat-code <codewithsifat4@gmail.com>
2025-03-10 02:53:33 +0600

    fixed the issue with the mongodbb driver part. removed 2 part

--- config/db.js
+++ config/db.js
@@ -2,13 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB Connected");
-  } catch (err) {
-    console.error(err);
+    await mongoose.connect(process.env.MONGO_URI); // ✅ Clean version
+
+    console.log("MongoDB Connected...");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
     process.exit(1);
   }
 };
